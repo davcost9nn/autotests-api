88241dc68e67b2081b8def7a7f78320b767e7b17
davcost9nn <davcost@yandex.ru>
2025-11-04 22:52:16 +0300

    Parametrize practice

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic import EmailStr
 
 from clients.users.public_user_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema,CreateUserResponseSchema
@@ -7,12 +8,14 @@ from http import HTTPStatus
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response
+from tools.fakers import fake
 
 
+@pytest.mark.parametrize('email',['mail.ru','gmail.com','example.com'])
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema()
+def test_create_user(public_users_client: PublicUsersClient,email: str):
+    request = CreateUserRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
