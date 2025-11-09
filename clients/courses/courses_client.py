b7e83f7684d02fdd19da3fc955d45209d65b54e2
davcost9nn <davcost@yandex.ru>
2025-11-10 00:33:40 +0300

    Practice with API tests part 2

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -10,24 +10,22 @@ from clients.private_http_builder import get_private_http_client, Authentication
 
 class CoursesClient(APIClient):
     def get_courses_api(self, query: GetCoursesQuerySchema) -> Response:
-        return self.get('/api/v1/courses',params=query)
+        return self.get('/api/v1/courses',params=query.model_dump(by_alias=True))
 
     def get_course_api(self,course_id: str) -> Response:
         return self.get(f'/api/v1/courses/{course_id}')
 
     def create_course_api(self, request: CreateCourseRequestSchema) -> Response:
-        return self.post('/api/v1/courses', json=request)
+        return self.post('/api/v1/courses', json=request.model_dump(by_alias=True))
 
     def update_course_api(self, course_id: str, request:UpdateCourseRequestSchema) -> Response:
-        return self.patch(f'/api/v1/courses/{course_id}', json=request)
+        return self.patch(f'/api/v1/courses/{course_id}', json=request.model_dump(by_alias=True))
 
     def delete_course_api(self,course_id: str) -> Response:
         return self.delete(f'/api/v1/courses/{course_id}')
 
     def create_course(self, request: CreateCourseRequestSchema) -> CreateCourseResponseSchema:
-        # Преобразуем с использованием алиасов (camelCase)
-        request_data = request.model_dump(by_alias=True)
-        response = self.create_course_api(request_data)
+        response = self.create_course_api(request)
         return CreateCourseResponseSchema.model_validate_json(response.text)
 
 def get_courses_client(user: AuthenticationUserSchema) -> CoursesClient:
--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -1,25 +1,38 @@
+from typing import Annotated
+
 from pydantic import Field,BaseModel,ConfigDict
 from clients.files.files_schema import FileSchema
 from clients.users.users_schema import UserSchema
 from tools.fakers import fake
+
 class CourseSchema(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
     id: str
     title: str
-    maxScore: int
-    minScore: int
+    max_score: int = Field(alias="maxScore")
+    min_score: int = Field(alias="minScore")
     description: str
-    previewFile: FileSchema
-    estimatedTime: str
-    createdByUser: UserSchema
+    preview_file: FileSchema = Field(alias="previewFile")
+    estimated_time: str = Field(alias="estimatedTime")
+    created_by_user: UserSchema = Field(alias="createdByUser")
 
 class CreateCourseResponseSchema(BaseModel):
     course: CourseSchema
 
 class GetCoursesQuerySchema(BaseModel):
-    userId: str
+    model_config = ConfigDict(populate_by_name=True)
+    user_id: str = Field(alias="userId")
+
+class GetCoursesResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на получение списка курсов.
+    """
+    courses: list[CourseSchema]
 
 class CreateCourseRequestSchema(BaseModel):
+
     model_config = ConfigDict(populate_by_name=True)
+
     title: str = Field(default_factory=fake.sentence)
     max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
     min_score: int = Field(alias="minScore", default_factory=fake.min_score)
@@ -28,9 +41,23 @@ class CreateCourseRequestSchema(BaseModel):
     preview_file_id: str = Field(alias="previewFileId", default_factory=fake.uuid4)
     created_by_user_id: str = Field(alias="createdByUserId", default_factory=fake.uuid4)
 
+
 class UpdateCourseRequestSchema(BaseModel):
+    """
+    Описание структуры запроса на обновление курса.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
     title: str | None = Field(default_factory=fake.sentence)
     max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
     min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
     description: str | None = Field(default_factory=fake.text)
     estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+
+class UpdateCourseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления курса.
+    """
+    course: CourseSchema
+
